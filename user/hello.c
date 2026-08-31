6c51aab3b37ffd78b434d7f882f92c656b2ad33f
MAKDA MEASHO <measho@unlv.nevada.edu>
2026-08-31 15:42:41 -0700

    My solution for Project 1 on xv6

---
+++ user/hello.c
@@ -0,0 +1,12 @@
+#include <kernel/types.h>
+#include <kernel/stat.h>
+#include <user/user.h>
+
+int main() 
+{
+    printf("Hello World!\n");
+    printf("Name: Makda Measho\n");
+    printf("Course & Section: CS 370, Section 1001\n");
+
+    exit(0);
+}
